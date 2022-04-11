a6d9aa63fe8928dab277d7f3d43d8425f5fd7108
Srivar Gillela <79170238+28gillelsr1@users.noreply.github.com>
2022-04-11 17:33:38 -0400

    Add files via upload

---
+++ gym.js
@@ -0,0 +1,30 @@
+Status = "";
+gym_image = "";
+
+function preload(){
+    gym_image = loadImage("gym.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(gym_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(gym_image,0,0,640,350);
+}
---
+++ street.js
@@ -0,0 +1,30 @@
+Status = "";
+street_image = "";
+
+function preload(){
+    street_image = loadImage("street.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(street_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(street_image,0,0,640,350);
+}
